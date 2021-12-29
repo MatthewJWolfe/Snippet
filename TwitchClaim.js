2090925179838b606b338c85c0cb1f6717a97573
Matthew Wolfe <mjw.wolfe@gmail.com>
2021-12-28 20:12:35 -0500

    Create TwitchClaim.js
    Auto claimer for twitch drops.
    For UserScript extentions

---
+++ TwitchClaim.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name     TwitchClaim
+// @version  1
+// @grant    none
+// ==/UserScript==
+setTimeout(function () {
+  console.log("we in");
+  var claimButton = document.querySelectorAll('[data-test-selector="DropsCampaignInProgressRewardPresentation-claim-button"]');
+  
+  if(claimButton.length == 1) {
+    console.log("Im a claimooooooor");
+  	claimButton[0].click();
+  }
+  else {
+  	console.log("not cooked");
+  }
+  location.reload();
+}, 300000);
