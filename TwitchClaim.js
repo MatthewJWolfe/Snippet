683b2c345e065f2efcf46915fd2d2de3a7b42da0
Matthew Wolfe <mjw.wolfe@gmail.com>
2021-12-29 18:58:37 -0500

    Update TwitchClaim.js

--- TwitchClaim.js
+++ TwitchClaim.js
@@ -3,18 +3,24 @@
 // @version  1
 // @grant    none
 // ==/UserScript==
+
+var reload_ = function() {
+  console.log("reloading");
+	location.reload();
+}
 setTimeout(function () {
+//confirm script running 
   console.log("we in");
   var claimButton = document.querySelectorAll('[data-test-selector="DropsCampaignInProgressRewardPresentation-claim-button"]');
-  
+//check for claim button
   if(claimButton.length == 1) {
     console.log("Im a claimooooooor");
-    var res = claimButton[0].click();
-    console.log(res);
+		claimButton[0].click();
     console.log("claimed");
   }
   else {
   	console.log("not cooked");
   }
-  location.reload();
+ //1 second pause to allow for claim
+  setTimeout(reload_, 1000);
 }, 300000);
